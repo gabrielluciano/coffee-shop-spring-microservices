a378a693f9c973ac296b55e75c027be6a6ec2e41
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-01-19 12:01:33 -0300

    fix(cart-service): fix exception message

--- cart-service/src/main/java/com/gabrielluciano/cartservice/exception/CartNotFoundException.java
+++ cart-service/src/main/java/com/gabrielluciano/cartservice/exception/CartNotFoundException.java
@@ -3,6 +3,6 @@ package com.gabrielluciano.cartservice.exception;
 public class CartNotFoundException extends ResourceNotFoundException {
 
     public CartNotFoundException(Long userId) {
-        super(String.format("Product with userId '%s' not found", userId));
+        super(String.format("Cart for user with id '%s' not found", userId));
     }
 }

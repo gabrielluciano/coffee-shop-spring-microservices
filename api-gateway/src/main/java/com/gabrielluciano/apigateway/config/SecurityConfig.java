fb846542fa6ac18e9b38c5a4cc608090063dfe00
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-02-01 14:39:00 -0300

    feat(api-gateway): add security rules to product service

--- api-gateway/src/main/java/com/gabrielluciano/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/gabrielluciano/apigateway/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.gabrielluciano.apigateway.model.Role;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -27,6 +28,9 @@ public class SecurityConfig {
         http.authorizeExchange(exchange -> exchange
                 .pathMatchers("/eureka/**").permitAll()
                 .pathMatchers("/api/v1/cart/**").hasRole(Role.USER.name())
+                .pathMatchers(HttpMethod.GET, "/api/v1/products", "/api/v1/products/").permitAll()
+                .pathMatchers(HttpMethod.GET, "/api/v1/products/{id}", "/api/v1/products/{id}/").permitAll()
+                .pathMatchers(HttpMethod.POST, "/api/v1/products", "/api/v1/products/").hasRole(Role.ADMIN.name())
                 .anyExchange().authenticated()
         );
         return http.build();

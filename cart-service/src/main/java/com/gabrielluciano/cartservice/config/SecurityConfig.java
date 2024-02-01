98c7be6b3c46e0ff7124172549ab443f27d66045
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-02-01 10:34:15 -0300

    feat(cart-service): add oauth2 resource server and basic config

---
+++ cart-service/src/main/java/com/gabrielluciano/cartservice/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.gabrielluciano.cartservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Value("${jwks.uri}")
+    private String jwksUri;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests
+                .anyRequest().authenticated()
+        );
+        http.oauth2ResourceServer(server -> server.
+                jwt(jwt -> jwt
+                        .jwkSetUri(jwksUri)
+                )
+        );
+        return http.build();
+    }
+}

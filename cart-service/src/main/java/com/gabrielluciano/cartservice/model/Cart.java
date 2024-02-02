9fb99b73df4e2b885932b49314508309f6b5a673
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-02-02 18:03:39 -0300

    refactor(cart-service): add equals and hashCode to models

--- cart-service/src/main/java/com/gabrielluciano/cartservice/model/Cart.java
+++ cart-service/src/main/java/com/gabrielluciano/cartservice/model/Cart.java
@@ -5,6 +5,7 @@ import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Getter
@@ -22,5 +23,18 @@ public class Cart {
 
     public void addItem(CartItem item) {
         this.items.add(item);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cart cart = (Cart) o;
+        return Objects.equals(id, cart.id) && Objects.equals(userId, cart.userId) && Objects.equals(items, cart.items) && Objects.equals(deletedAt, cart.deletedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId, items, deletedAt);
     }
 }
--- cart-service/src/main/java/com/gabrielluciano/cartservice/model/CartItem.java
+++ cart-service/src/main/java/com/gabrielluciano/cartservice/model/CartItem.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -20,5 +22,18 @@ public class CartItem {
 
     public void increaseQuantityBy(Integer quantity) {
         this.quantity += quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CartItem cartItem = (CartItem) o;
+        return Objects.equals(productId, cartItem.productId) && Objects.equals(quantity, cartItem.quantity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, quantity);
     }
 }

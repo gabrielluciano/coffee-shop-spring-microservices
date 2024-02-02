1afa6e0820e81fa18a84190871b2e4b1b27c6e52
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-02-02 18:02:42 -0300

    feat(cart-service): add method authorization rules in CartController and disable csrf

--- cart-service/src/main/java/com/gabrielluciano/cartservice/config/SecurityConfig.java
+++ cart-service/src/main/java/com/gabrielluciano/cartservice/config/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
         http.authorizeHttpRequests(requests -> requests
                 .anyRequest().authenticated()
         );
--- cart-service/src/main/java/com/gabrielluciano/cartservice/controller/CartController.java
+++ cart-service/src/main/java/com/gabrielluciano/cartservice/controller/CartController.java
@@ -5,6 +5,7 @@ import com.gabrielluciano.cartservice.dto.CartResponse;
 import com.gabrielluciano.cartservice.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -17,16 +18,19 @@ public class CartController {
     private final CartService cartService;
 
     @PostMapping("/add")
+    @PreAuthorize("#cartRequest.userId.toString() == principal.claims['userId']")
     public CartResponse addItem(@RequestBody CartRequest cartRequest) {
         return cartService.addItem(cartRequest);
     }
 
     @GetMapping("/{userId}")
+    @PreAuthorize("#userId.toString() == principal.claims['userId']")
     public CartResponse getCart(@PathVariable UUID userId) {
-       return cartService.getCart(userId);
+        return cartService.getCart(userId);
     }
 
     @DeleteMapping("/{userId}")
+    @PreAuthorize("#userId.toString() == principal.claims['userId']")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void clearCart(@PathVariable UUID userId) {
         cartService.clearCart(userId);
